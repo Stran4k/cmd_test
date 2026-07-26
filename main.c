7aba1fdd9c2d8cde6842cc5f564c00cc04dc4041
Stran4k <vitalik-sik@mail.ru>
2026-07-26 17:27:00 +0300

    Second commit

--- main.c
+++ main.c
@@ -12,8 +12,6 @@ printf("%s\r\n","\nEnd...");
 /*
  * 
  * 
- * 
- * 
  * gcc main.c -o test1.exe 
  *  сбока .exe с именем 
  * 
@@ -22,11 +20,17 @@ printf("%s\r\n","\nEnd...");
  * out : -a -s ds End...
  *  вывод test1.exe
  * 
+ * ls 
+ *  Просмотр файлов в директории
+ * 
  * cd D:
  *  Открывает искомый диск
  * 
- * cd \_Visual_Studio_Project\C
- *  Открвть католог (папку/и)
+ * cd cmd_testing
+ *  Открыть папку
+ * 
+ * cd \_Visual_Studio_Project\C\cmd_testing
+ *  Открывть католог (папку/и) по заданному пути
  * 
  * cd .. 
  *  Возврат в родительскую папку
@@ -34,7 +38,52 @@ printf("%s\r\n","\nEnd...");
  * 
  *          Git
  * 
+ * git init
  * 
+ * git add *.c
+ * 
+ * git add LICENSE
+ * 
+ * git commit -m "first commit Testing terminal"
+ * 
+ * 
+ * second  commit
+ * 
+ * 
+These are common Git commands used in various situations:
+
+start a working area (see also: git help tutorial)
+   clone      Clone a repository into a new directory
+   init       Create an empty Git repository or reinitialize an existing one
+
+work on the current change (see also: git help everyday)
+   add        Add file contents to the index
+   mv         Move or rename a file, a directory, or a symlink
+   restore    Restore working tree files
+   rm         Remove files from the working tree and from the index
+
+examine the history and state (see also: git help revisions)
+   bisect     Use binary search to find the commit that introduced a bug
+   diff       Show changes between commits, commit and working tree, etc
+   grep       Print lines matching a pattern
+   log        Show commit logs
+   show       Show various types of objects
+   status     Show the working tree status
+
+grow, mark and tweak your common history
+   backfill   Download missing objects in a partial clone
+   branch     List, create, or delete branches
+   commit     Record changes to the repository
+   merge      Join two or more development histories together
+   rebase     Reapply commits on top of another base tip
+   reset      Set `HEAD` or the index to a known state
+   switch     Switch branches
+   tag        Create, list, delete or verify tags
+
+collaborate (see also: git help workflows)
+   fetch      Download objects and refs from another repository
+   pull       Fetch from and integrate with another repository or a local branch
+   push       Update remote refs along with associated objects
  * 
  * 
  */
